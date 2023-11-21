b63a8468770f368fcfd9f09a5f3e31fbbe98fa9f
Durgesh Kavate <125905973+DurgeshKavate003@users.noreply.github.com>
2023-11-21 22:06:43 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,51 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] sortedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 2;
+
+        int left = 0;
+        int right = sortedArray.length - 1;
+
+        boolean iterativeResult = binarySearchIterative(sortedArray, target);
+        System.out.println("Iterative Binary Search Result: " + iterativeResult);
+
+        boolean recursiveResult = binarySearchRecursive(sortedArray, target, left, right);
+        System.out.println("Recursive Binary Search Result: " + recursiveResult);
+    }
+
+    public static boolean binarySearchIterative(int[] array, int x) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + ((right - left) / 2);
+    
+            if (array[mid] == x) {
+                return true;
+            } else if (x < array[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+
+    public static boolean binarySearchRecursive(int[] array, int x, int left, int right) {
+        if (left > right) {
+            return false;
+        }
+
+        int mid = left + ((right - left) / 2);
+
+        if (array[mid] == x) {
+            return true;
+        } else if (x < array[mid]) {
+            return binarySearchRecursive(array, x, left, mid - 1);
+        } else {
+            return binarySearchRecursive(array, x, mid + 1, right);
+        }
+    }
+
+}
